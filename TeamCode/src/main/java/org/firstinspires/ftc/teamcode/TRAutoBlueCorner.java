f59eff0aaa6384463e8743644517e4ae27dea2f7
Blake Sanders <ac3.train3r.blak3@gmail.com>
2017-10-19 16:40:19 -0500

    Commented out jewel logic and added experimental Vuforia code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TRAutoBlueCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TRAutoBlueCorner.java
@@ -5,6 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 /**
  * Created by swamp on 10/17/2017.
  */
@@ -14,12 +21,39 @@ public class TRAutoBlueCorner extends LinearOpMode {
 
     private TileRunnerREV hardware = new TileRunnerREV();
 
+    private VuforiaLocalizer vuforia;
+
     @Override
     public void runOpMode() {
-        telemetry.addLine("Initializing telemetry... do not press play.");
+        telemetry.addLine("Initializing hardware and Vuforia... do not press play.");
         telemetry.update();
 
+        // Initialize hardware class
         hardware.init(hardwareMap);
+
+
+        // Set up the Vuforia parameters
+        // (Comment out first line and cameraMonitorViewId parameter on second line to remove screen display)
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // Set the license key and the camera being used
+        parameters.vuforiaLicenseKey = "AQEp+gX/////AAAAGabZ3yaKT0WLtofdjrrGznRKhqhzUCjAtaxsfr96aQv7kVlGcd6NUnv2Ic89/rJ/yPFvXrDDIWqGfpXvAhqVO94fs5EYBWUzB8qCBfTJ6U1Lmo15bBZ5/tz0iMkFc3ZX27xBTdIJ6C3zTIna1hErBvkeKpRI8nMwygPulWQej4jCaomF600Z9t9ZZZtQCH54bgqLmzMRIwZYOxCzcwh+nfP7teg9JtwI3NSUHmL2zkIiRYzwo53vv3+kv3CdzLnfiK/6ReAW6S/p9hO0ENCIcWJDUGgM4KDBW1aewp6OTpFt34D2ZIzop63/+ediGz8PJw3pcrRAuKEDQ/p1h7GAVHw8vbWgW1iTOkevHSv4bcp8\n";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        // Create a VuforiaLocalizer
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        // Get all the vision targets
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+
+        // Get the template target
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+        // Set the name (for debugging purposes)
+        relicTemplate.setName("RelicVuMarkTemplate");
+
+
 
         telemetry.addLine("Hardware Initialized.");
         telemetry.addLine("Press the play button to start.");
@@ -27,37 +61,52 @@ public class TRAutoBlueCorner extends LinearOpMode {
 
         waitForStart();
 
-        hardware.jewelServo.setPosition(hardware.ARM_DOWN);
-        telemetry.addData("Blue", hardware.colorSensor.blue());
-        telemetry.addData("Red", hardware.colorSensor.red());
-        telemetry.update();
-        sleep(2000);
 
-        if(hardware.colorSensor.blue() > hardware.colorSensor.red()) {
-            telemetry.addData("Blue", hardware.colorSensor.blue());
-            telemetry.addData("Red", hardware.colorSensor.red());
-            telemetry.addLine("Left jewel was blue. Driving forwards...");
-            telemetry.update();
-            sleep(2000);
+        // Start looking for the vision targets
+        relicTrackables.activate();
 
-            driveInches((float)0.7, (float)3.0);
-        }
-        else {
-            telemetry.addData("Blue", hardware.colorSensor.blue());
-            telemetry.addData("Red", hardware.colorSensor.red());
-            telemetry.addLine("Left jewel was red. Driving backwards...");
-            telemetry.update();
-            sleep(2000);
+        //////////////////////
+        // JEWEL LOGIC
 
-            driveInches((float)0.7, (float)-3.0);
-        }
+//        hardware.jewelServo.setPosition(hardware.ARM_DOWN);
+//        telemetry.addData("Blue", hardware.colorSensor.blue());
+//        telemetry.addData("Red", hardware.colorSensor.red());
+//        telemetry.update();
+//        sleep(2000);
+//
+//        if(hardware.colorSensor.blue() > hardware.colorSensor.red()) {
+//            telemetry.addData("Blue", hardware.colorSensor.blue());
+//            telemetry.addData("Red", hardware.colorSensor.red());
+//            telemetry.addLine("Left jewel was blue. Driving forwards...");
+//            telemetry.update();
+//            sleep(2000);
+//
+//            driveInches((float)0.7, (float)3.0);
+//        }
+//        else {
+//            telemetry.addData("Blue", hardware.colorSensor.blue());
+//            telemetry.addData("Red", hardware.colorSensor.red());
+//            telemetry.addLine("Left jewel was red. Driving backwards...");
+//            telemetry.update();
+//            sleep(2000);
+//
+//            driveInches((float)0.7, (float)-3.0);
+//        }
+//
+//        sleep(3000);
+//        hardware.jewelServo.setPosition(hardware.ARM_UP);
 
-        sleep(3000);
-        hardware.jewelServo.setPosition(hardware.ARM_UP);
-
-
+        // JEWEL LOGIC END
+        //////////////////////
 
         while(opModeIsActive()) {
+
+            // Try to find the vuMark
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
+            telemetry.addLine("Visible vision target:");
+            telemetry.addLine(vuMarkTelemetry(vuMark));
+            telemetry.addLine();
             telemetry.addLine("Color Sensor Values:");
             telemetry.addData("Red", hardware.colorSensor.red());
             telemetry.addData("Blue", hardware.colorSensor.blue());
@@ -87,5 +136,18 @@ public class TRAutoBlueCorner extends LinearOpMode {
 
     public void driveInches(float power, float inches) {
         driveEncoderCounts(power, (int)(inches * hardware.COUNTS_PER_INCH));
+    }
+
+    public String vuMarkTelemetry(RelicRecoveryVuMark mark) {
+        switch(mark) {
+            case LEFT:
+                return "Left";
+            case CENTER:
+                return "Center";
+            case RIGHT:
+                return "Right";
+            default:
+                return "None";
+        }
     }
 }
