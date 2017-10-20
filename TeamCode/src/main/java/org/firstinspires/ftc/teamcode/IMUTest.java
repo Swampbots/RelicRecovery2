bd9f832119d88272a908bb50bf55ed182b277cd1
Blake Sanders <ac3.train3r.blak3@gmail.com>
2017-10-20 12:22:17 -0400

    Added a modified version of the IMU test class from sample code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Locale;
+
+/**
+ * Created by SwampbotsAdmin on 10/20/2017.
+ */
+
+public class IMUTest extends LinearOpMode {
+
+    TileRunnerREV hardware = new TileRunnerREV();
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+    public void runOpMode() {
+
+        // Set up our telemetry dashboard
+        composeTelemetry();
+
+        waitForStart();
+
+        // Start the logging of measured acceleration
+        hardware.imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        while(opModeIsActive()) {
+            // Handle speed modifiers
+            if(gamepad1.left_bumper)        hardware.driverSpeedMod = hardware.FAST;
+            else if(gamepad1.right_bumper)  hardware.driverSpeedMod = hardware.SLOW;
+            else                            hardware.driverSpeedMod = hardware.NORMAL;
+
+            hardware.linearDrive(
+                    (float)(gamepad1.left_stick_y * hardware.driverSpeedMod),
+                    (float)(gamepad2.right_stick_y * hardware.driverSpeedMod));
+
+            telemetry.update();
+        }
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+    // Telemetry Configuration
+    //----------------------------------------------------------------------------------------------
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = hardware.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = hardware.imu.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return hardware.imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return hardware.imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
