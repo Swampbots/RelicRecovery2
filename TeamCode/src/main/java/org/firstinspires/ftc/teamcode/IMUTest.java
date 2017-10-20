58f7dcd54f4793c8afda8ba259692d7a7403c2e6
Blake Sanders <ac3.train3r.blak3@gmail.com>
2017-10-20 15:07:53 -0400

    Added testing controls to IMUTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
@@ -25,12 +26,16 @@ public class IMUTest extends LinearOpMode {
 
     TileRunnerREV hardware = new TileRunnerREV();
 
+    BNO055IMU imu;
+
     // State used for updating telemetry
     Orientation angles;
     Acceleration gravity;
 
-    public void runOpMode() {
+    final int DEGREES = 73;
+    int targetAngle;
 
+    public void runOpMode() {
         // IMU parameters
         BNO055IMU.Parameters IMUParameters = new BNO055IMU.Parameters();
         IMUParameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
@@ -40,29 +45,127 @@ public class IMUTest extends LinearOpMode {
         IMUParameters.loggingTag          = "IMU";
         IMUParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-        hardware.imu.initialize(IMUParameters);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(IMUParameters);
+
+        hardware.init(hardwareMap);
 
 
 
         // Set up our telemetry dashboard
         composeTelemetry();
 
-
+//
+//        // Start the logging of measured acceleration
+//        //hardware.imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+//
+//        while(opModeIsActive()) {
+//            // Handle speed modifiers
+//            if(gamepad1.left_bumper)        hardware.driverSpeedMod = hardware.FAST;
+//            else if(gamepad1.right_bumper)  hardware.driverSpeedMod = hardware.SLOW;
+//            else                            hardware.driverSpeedMod = hardware.NORMAL;
+//
+//            hardware.linearDrive(
+//                    (float)(gamepad1.left_stick_y * hardware.driverSpeedMod),
+//                    (float)(gamepad2.right_stick_y * hardware.driverSpeedMod));
+//
+//            telemetry.update();
 
         waitForStart();
-
-        // Start the logging of measured acceleration
-        //hardware.imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-
         while(opModeIsActive()) {
-            // Handle speed modifiers
             if(gamepad1.left_bumper)        hardware.driverSpeedMod = hardware.FAST;
             else if(gamepad1.right_bumper)  hardware.driverSpeedMod = hardware.SLOW;
             else                            hardware.driverSpeedMod = hardware.NORMAL;
 
             hardware.linearDrive(
                     (float)(gamepad1.left_stick_y * hardware.driverSpeedMod),
-                    (float)(gamepad2.right_stick_y * hardware.driverSpeedMod));
+                    (float)(gamepad1.right_stick_y * hardware.driverSpeedMod));
+
+            if(gamepad1.dpad_left) {
+
+                hardware.leftDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                hardware.leftDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                hardware.rightDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                hardware.rightDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+                telemetry.addLine(String.format("Turning to %s degrees...", DEGREES));
+                telemetry.addLine(String.format("Start angle: %s", angles.firstAngle));
+                telemetry.update();
+                sleep(1500);
+
+                hardware.linearDrive((float)0.4, (float)-0.4);
+
+                while(opModeIsActive() && angles.firstAngle < DEGREES) {
+                    telemetry.addLine(String.format("Turning to %s degrees...", DEGREES));
+                    telemetry.addLine(String.format("Current angle: %s", angles.firstAngle));
+                    telemetry.update();
+                }
+                hardware.linearDrive(0);
+
+                telemetry.addLine("Finished with turn.");
+                telemetry.update();
+                sleep(2000);
+            } else if(gamepad1.dpad_right) {
+                telemetry.addLine(String.format("Turning to %s degrees...", DEGREES));
+                telemetry.addLine(String.format("Starting angle: %s", angles.firstAngle));
+                telemetry.update();
+
+                hardware.linearDrive((float)-0.5, (float)0.5);
+
+                while(opModeIsActive() && angles.firstAngle > -DEGREES) {
+                    telemetry.addLine(String.format("Turning to %s degrees...", DEGREES));
+                    telemetry.addLine(String.format("Current angle: %s", angles.firstAngle));
+                    telemetry.update();
+                }
+                hardware.linearDrive(0);
+
+                telemetry.addLine("Finished with turn.");
+                telemetry.update();
+                sleep(2000);
+            } else if(gamepad1.dpad_up) {
+                telemetry.addLine(String.format("Turning %s degrees...", DEGREES));
+                telemetry.addData("Starting angle", angles.firstAngle);
+                telemetry.addData("Target angle", angles);
+                telemetry.update();
+
+                targetAngle = (int)(angles.firstAngle + DEGREES);
+
+                hardware.linearDrive((float)-0.5, (float)0.5);
+
+                while( opModeIsActive() && angles.firstAngle < targetAngle) {
+                    telemetry.addLine(String.format("Turning %s degrees...", DEGREES));
+                    telemetry.addData("Current angle", angles.firstAngle);
+                    telemetry.update();
+                }
+
+                hardware.linearDrive(0);
+
+                telemetry.addLine("Finished with turn.");
+                telemetry.update();
+                sleep(2000);
+            } else if(gamepad1.dpad_down) {
+                telemetry.addLine(String.format("Turning %s degrees...", DEGREES));
+                telemetry.addData("Starting angle", angles.firstAngle);
+                telemetry.update();
+
+                targetAngle = (int)(angles.firstAngle - DEGREES);
+
+                hardware.linearDrive((float)-0.5, (float)0.5);
+
+                while(opModeIsActive() && angles.firstAngle > targetAngle) {
+                    telemetry.addLine(String.format("Turning %s degrees...", DEGREES));
+                    telemetry.addData("Current angle", angles.firstAngle);
+                    telemetry.update();
+                }
+
+                hardware.linearDrive(0);
+
+                telemetry.addLine("Finished with turn.");
+                telemetry.update();
+                sleep(2000);
+            }
 
             telemetry.update();
         }
@@ -82,20 +185,20 @@ public class IMUTest extends LinearOpMode {
             // Acquiring the angles is relatively expensive; we don't want
             // to do that in each of the three items that need that info, as that's
             // three times the necessary expense.
-            angles   = hardware.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-            gravity  = hardware.imu.getGravity();
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
         }
         });
 
         telemetry.addLine()
                 .addData("status", new Func<String>() {
                     @Override public String value() {
-                        return hardware.imu.getSystemStatus().toShortString();
+                        return imu.getSystemStatus().toShortString();
                     }
                 })
                 .addData("calib", new Func<String>() {
                     @Override public String value() {
-                        return hardware.imu.getCalibrationStatus().toString();
+                        return imu.getCalibrationStatus().toString();
                     }
                 });
 
